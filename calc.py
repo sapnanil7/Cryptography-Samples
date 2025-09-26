45ebda1df702e4ff6c09e71ad58e6f119942597d
Sapnanil Das <44121115+sapnanil7@users.noreply.github.com>
2025-09-27 04:19:11 +0530

    day 1

---
+++ calc.py
@@ -0,0 +1,4 @@
+item_price = 24.99
+quantity = 3
+total_cost = item_price * quantity
+print("Total cost:", total_cost)
---
+++ loop2.py
@@ -0,0 +1,8 @@
+count = 5
+while (count >= 0):
+    if(count == 1):
+        print("Lift-off in 1 second!")
+    else:
+        print(f"T-minus {count} seconds")
+    count-=1
+
